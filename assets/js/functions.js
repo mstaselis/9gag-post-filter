77a0f5e7522018bc9a31de4b061461343859d284
mindaugas <mindaugas.staselis@iraida.lt>
2025-04-30 16:51:13 +0300

    intermediate work

---
+++ assets/js/functions.js
@@ -0,0 +1,23 @@
+function createAddObserver(targetElement, callback, options = { childList: true }) {
+  const observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+      mutation.addedNodes.forEach((addedNode) => {
+        callback(addedNode);
+      });
+    });
+  });
+  observer.observe(targetElement, options);
+
+  return observer;
+}
+
+const getNameFromMenu = async (art_id) => {
+  if (document.querySelector("#" + art_id + " .uikit-popup-menu") !== null) {
+    //desktop
+    let el = document.querySelector("#" + art_id + " .uikit-popup-menu");
+    await el.querySelector(".button").click();
+    let name = await [...el.querySelectorAll(".menu a")].at(-1).text.split("@")[1];
+    el.querySelector(".button").click();
+    return name;
+  }
+};
