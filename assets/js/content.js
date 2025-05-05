830dd91adf2fabc88af538c140a139908e25db63
mindaugas <mindaugas.staselis@iraida.lt>
2025-05-05 17:01:32 +0300

    refactorings

--- assets/js/content.js
+++ assets/js/content.js
@@ -17,11 +17,13 @@ if (containerElement && !window.location.pathname.includes("/u/")) {
     { childList: true, subtree: true },
   );
 
-  //filter first records
-  var firstPosts = $(".stream-container");
-  if (firstPosts) {
-    filter(firstPosts);
-  }
+  setTimeout(() => {
+    //filter first records
+    var firstPosts = $(".stream-container");
+    if (firstPosts) {
+      filter(firstPosts);
+    }
+  }, 1000);
 }
 
 function initialize() {
@@ -78,75 +80,76 @@ function filter(addedNode) {
         credentials: "include",
       });
       const json = await response.json();
-      let creatorCreation = json.data.profile.creationTs;
+      if (json) {
+        let creatorCreation = json.data.profile.creationTs;
 
-      let now = Date.now() / 1000;
-      let diff = now - creatorCreation;
-      diff = diff / 86400; //in days
-      diff = parseInt(diff);
+        let now = Date.now() / 1000;
+        let diff = now - creatorCreation;
+        diff = diff / 86400; //in days
+        diff = parseInt(diff);
 
-      if (settings.min_days > diff) {
-        post.hide();
-        post.addClass("filtered");
-        return;
-      }
-      if (post.find(".ui-post-creator").length) post.find(".ui-post-creator").append("| " + diff + " days");
-      else {
-        post.find(".post-header__left").append("<span>| " + diff + " days</span>");
-        post.find(".post-meta.mobile").append("<span>| " + diff + " days</span>");
-      }
-
-      if (settings.spammers && json.data.posts.length >= 10) {
-        let posts = json.data.posts;
-        let postDiff = [];
-        for (let i = 0; i < posts.length; i++) {
-          let creationTs = posts[i].creationTs;
-          if (i == 0)
-            postDiff[i] = (Date.now() / 1000 - creationTs) / 3600; //as hours
-          else postDiff[i] = (posts[i - 1].creationTs - creationTs) / 3600;
+        if (settings.min_days > diff) {
+          post.hide();
+          post.addClass("filtered");
+          return;
         }
-        const average = (array) => array.reduce((a, b) => a + b) / array.length;
-        let diffAve = average(postDiff);
-        let diffset = !isNaN(settings.spammers_hours) ? settings.spammers_hours : 12;
+        if (post.find(".ui-post-creator").length) post.find(".ui-post-creator").append("| " + diff + " days");
+        else {
+          post.find(".post-header__left").append("<span>| " + diff + " days</span>");
+          post.find(".post-meta.mobile").append("<span>| " + diff + " days</span>");
+        }
 
-        if (diffAve < diffset) {
-          if (settings.hide_spammers) {
-            post.hide();
-          } else {
-            if (post.find(".ui-post-creator").length) post.find(".ui-post-creator").append(`<span class="spammer-label">| SPAMMER</span>`);
-            else {
-              post.find(".post-header__left").append(`<span class="spammer-label">| SPAMMER</span>`);
-              post.find(".post-meta.mobile").append(`<span class="spammer-label">| SPAMMER</span>`);
+        if (settings.spammers && json.data.posts.length >= 10) {
+          let posts = json.data.posts;
+          let postDiff = [];
+          for (let i = 0; i < posts.length; i++) {
+            let creationTs = posts[i].creationTs;
+            if (i == 0)
+              postDiff[i] = (Date.now() / 1000 - creationTs) / 3600; //as hours
+            else postDiff[i] = (posts[i - 1].creationTs - creationTs) / 3600;
+          }
+          const average = (array) => array.reduce((a, b) => a + b) / array.length;
+          let diffAve = average(postDiff);
+          let diffset = !isNaN(settings.spammers_hours) ? settings.spammers_hours : 12;
+
+          if (diffAve < diffset) {
+            if (settings.hide_spammers) {
+              post.hide();
+            } else {
+              if (post.find(".ui-post-creator").length) post.find(".ui-post-creator").append(`<span class="spammer-label">| SPAMMER</span>`);
+              else {
+                post.find(".post-header__left").append(`<span class="spammer-label">| SPAMMER</span>`);
+                post.find(".post-meta.mobile").append(`<span class="spammer-label">| SPAMMER</span>`);
+              }
             }
           }
         }
-      }
 
-      let post_id = [...post.find("header a")].at(-1).href.split("/").at(-1);
-      console.log("post_id " + post_id);
+        let post_id = [...post.find("header a")].at(-1).href.split("/").at(-1);
 
-      let posts = json.data.posts;
-      let downvotes = null;
-      let upvotes = null;
+        let posts = json.data.posts;
+        let downvotes = null;
+        let upvotes = null;
 
-      for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id === post_id) {
-          downvotes = posts[i].downVoteCount;
-          upvotes = posts[i].upVoteCount;
-          break;
-        }
-      }
-
-      if (downvotes !== null) {
-        if (settings.more_downvotes && downvotes >= upvotes) {
-          post.hide();
+        for (let i = 0; i < posts.length; i++) {
+          if (posts[i].id === post_id) {
+            downvotes = posts[i].downVoteCount;
+            upvotes = posts[i].upVoteCount;
+            break;
+          }
         }
 
-        if (settings.always_display_upvotes && post.find(".upvote").eq(1).html() == "•") {
-          post.find(".upvote").eq(1).html(upvotes);
+        if (downvotes !== null) {
+          if (settings.more_downvotes && downvotes >= upvotes) {
+            post.hide();
+          }
+
+          if (settings.always_display_upvotes && post.find(".upvote").eq(1).html() == "•") {
+            post.find(".upvote").eq(1).html(upvotes);
+          }
+          post.find(".post-vote").append(`<span class="post-vote__text downvote upvote">${downvotes}</span>`);
+          post.find(".downvote.grouped").after(`<span class="post-vote__text downvote">${downvotes}</span>`);
         }
-        post.find(".post-vote").append(`<span class="post-vote__text downvote upvote">${downvotes}</span>`);
-        post.find(".downvote.grouped").after(`<span class="post-vote__text downvote">${downvotes}</span>`);
       }
     }
 
